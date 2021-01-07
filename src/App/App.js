e8c311468b127da6a3e6b8d9517d08db268c86ed
ShotAlex <shot.aleks@gmail.com>
2021-01-07 19:02:29 +0300

    feat: add HOC Layout

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import Layout from "../HOC/Layout/Layout";
+import classes from './App.module.css'
+
+function App() {
+  return (
+    <div className={classes.app}>
+        <Layout />
+    </div>
+  );
+}
+
+export default App;
