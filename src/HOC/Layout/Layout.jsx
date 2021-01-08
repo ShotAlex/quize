70ad443d3ab799df90a85e2363e2fee1c1af9d98
ShotAlex <shot.aleks@gmail.com>
2021-01-08 03:07:16 +0300

    feat: add burger menu

--- src/HOC/Layout/Layout.jsx
+++ src/HOC/Layout/Layout.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import classes from './Layout.module.css'
-import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
+
 
 class Layout extends  React.Component{
   state = {
     menu: false
   }
+
   onToggleMenuHandler = () => {
     this.setState({
       menu: !this.state.menu
     })
   }
 
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false
+    })
+  }
+
   render() {
     return (
       <div className={classes.Layout}>
+
+        <Drawer
+          onClose={this.menuCloseHandler}
+          isOpen={this.state.menu}
+        />
+
         <MenuToggle
           onToggle={this.onToggleMenuHandler}
           isOpen={this.state.menu}
