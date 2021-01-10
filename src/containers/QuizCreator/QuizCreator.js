fee41c596ef013bb76840806d40a3c71645d8956
ShotAlex <shot.aleks@gmail.com>
2021-01-10 16:11:10 +0300

    feat: add basic template for Create quiz page

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,43 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import Button from '../../components/UI/Button/Button'
+import classes from './QuizCreator.module.css'
 
 class QuizCreator extends Component {
+
+  state = {
+    quiz: [],
+    formControls: {
+      question: '',
+      option1: '',
+      option2: '',
+      option3: '',
+      option4: ''
+    }
+  }
+
+  submitHandler = event => event.preventDefault()
+  addQuestionHandler = () => {}
+  createQuizHandler = () => {}
+
   render() {
     return (
-      <div>
-        QuizCreator
+      <div className={classes.QuizCreator}>
+        <h1>QuizCreator</h1>
+
+        <form onSubmit={this.submitHandler}>
+
+          <input type="text"/>
+          <hr/>
+          <input type="text"/>
+          <input type="text"/>
+          <input type="text"/>
+          <input type="text"/>
+          <select/>
+          <hr/>
+
+          <Button type='primary' onClick={this.addQuestionHandler} > Add question </Button>
+          <Button type='success' onClick={this.createQuizHandler} > Create quiz </Button>
+        </form>
       </div>
     );
   }
